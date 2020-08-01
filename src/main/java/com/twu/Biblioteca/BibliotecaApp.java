f518644a911ac6d300d2eba10ca4c5ffc29fee49
fanyue4 <fan.yue@hotmail.com>
2020-08-01 10:50:31 +0800

    modify interface

--- src/main/java/com/twu/Biblioteca/BibliotecaApp.java
+++ src/main/java/com/twu/Biblioteca/BibliotecaApp.java
@@ -2,59 +2,49 @@ package com.twu.Biblioteca;
 
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
 public class BibliotecaApp {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Library library = new Library();
         System.out.println(library.welcome());
 
-        System.out.println("\nMenu:"
-                + "\n"
-                + "List of Books (Please enter number 1)"
-                + "\n"
-                + "Quit (Please enter number number 0)"
-                + "\n");
-
-        Scanner scanner = new Scanner(System.in);
         while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("\nMenu:"
+                    + "\n"
+                    + "List of Books (Please enter number 1)"
+                    + "\n"
+                    + "Quit (Please enter number number 0)");
             System.out.print("Please enter your number:");
             int number = scanner.nextInt();
             if (number == 0) {
                 return;
             } else if (number == 1) {
+                System.out.print("\n");
                 System.out.println(library.generateBookList());
-                System.out.println("Checkout (Please enter number 2)"
-                        +"\nReturn (Please enter number 3)");
+                String messageOfCheckout;
+                String messageOfReturn;
+                do {
+                    System.out.print("Please enter title of book to checkout:");
+                    String title = new Scanner(System.in).nextLine();
+                    messageOfCheckout = library.checkOutBook(title);
+                    System.out.println(messageOfCheckout);
+                } while (messageOfCheckout.equals("Sorry, that book is not available"));
+
+                TimeUnit.SECONDS.sleep(1);
+
+                do {
+                    System.out.print("\nPlease enter title of book to return:");
+                    String titleOfReturned = new Scanner(System.in).nextLine();
+                    messageOfReturn = library.returnBook(titleOfReturned);
+                    System.out.println(messageOfReturn);
+                } while (messageOfReturn.equals("That is not a valid book to return"));
             } else {
                 System.out.println("Please select a valid option!");
             }
-        }
-        /*
-        if (scanner.nextLine().equals("1")) {
-            System.out.println(library.generateBookList());
-            System.out.println("Checkout (Please enter number 2)"
-                    +"\nReturn (Please enter number 3)"
-                    +"\nPlease enter your choice:");
-            if (scanner.nextLine().equals("2")) {
-                System.out.println("\nPlease enter title of book to checkout:");
-                String title = scanner.nextLine();
-                String message = library.checkOutBook(title);
-                System.out.println(message);
-            }
 
-            if (scanner.nextLine().equals("3")) {
-                System.out.println("\nPlease enter title of book to return:");
-                String title = scanner.nextLine();
-                String message = library.returnBook(title);
-                System.out.println(message);
-            }
-        } else if (scanner.nextLine().equals("0")){
-            System.exit(0);
-        } else {
-            System.out.println("Please select a valid option:");
-            new Scanner(System.in);
+            TimeUnit.SECONDS.sleep(1);
         }
-
-         */
     }
 }

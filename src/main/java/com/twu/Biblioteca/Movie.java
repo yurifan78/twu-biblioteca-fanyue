22747dab894b44f183a5a1726760230dfc10996f
fanyue4 <fan.yue@hotmail.com>
2020-08-02 15:24:17 +0800

    create movie class

---
+++ src/main/java/com/twu/Biblioteca/Movie.java
@@ -0,0 +1,71 @@
+package com.twu.Biblioteca;
+
+public class Movie{
+    private String name;
+    private String year;
+    private String director;
+    private int rate;
+    private Status status;
+
+    public Movie(String name, String year, String director, int rate, Status status) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rate = rate;
+        this.status = status;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public int getRate() {
+        return rate;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public void setRate(int rate) {
+        this.rate = rate;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return name +
+                ", " +
+                year +
+                ", " +
+                director +
+                ", " +
+                rate +
+                ", " +
+                status;
+    }
+}
+
